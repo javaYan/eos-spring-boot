465f952c06e10f7137fc019abaedc92a991eb79e
javaYan <834595596@qq.com>
2017-02-28 18:51:46 +0800

    file stream

---
+++ eos-java-practice/src/main/java/eos/java/practice/file/FileOperator.java
@@ -0,0 +1,11 @@
+package eos.java.practice.file;
+
+import java.io.File;
+
+/**
+ * Created by yanyuyu on 2017/2/28.
+ */
+public class FileOperator {
+    private File file;
+
+}
---
+++ eos-java-practice/src/main/java/eos/java/practice/file/StreamOperator.java
@@ -0,0 +1,8 @@
+package eos.java.practice.file;
+
+/**
+ * Created by yanyuyu on 2017/2/28.
+ */
+public class StreamOperator {
+
+}
