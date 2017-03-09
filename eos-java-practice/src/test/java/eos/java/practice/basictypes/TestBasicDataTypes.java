171d4246443f0f32b7f84bc0c2bf835774b2848e
javaYan <834595596@qq.com>
2017-03-09 13:24:23 +0800

    basic data type test

---
+++ eos-java-practice/src/test/java/eos/java/practice/basictypes/TestBasicDataTypes.java
@@ -0,0 +1,15 @@
+package eos.java.practice.basictypes;
+
+import org.junit.Test;
+
+/**
+ * Created by yanyuyu on 2017/3/8.
+ */
+public class TestBasicDataTypes {
+    @Test
+    public void testfloat() {
+        float f1 = 0.34f; // right
+        float f2 = (float) 0.35; // right
+        //float f3 = 0.4; // wrong
+    }
+}
