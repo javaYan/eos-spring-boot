7ad5342e92cc9da26dbb80aa145db3b2863dad2c
javaYan <834595596@qq.com>
2017-03-22 22:35:05 +0800

    cross

--- eos-guava-eventbus/src/main/java/eos/guava/eventbus/controller/CrossController.java
+++ eos-guava-eventbus/src/main/java/eos/guava/eventbus/controller/CrossController.java
@@ -1,10 +1,13 @@
 package eos.guava.eventbus.controller;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,13 +20,20 @@ import java.util.Map;
 public class CrossController {
 
     @RequestMapping(value="cross",method=RequestMethod.GET)
-    public Map<String,Object> doGet(HttpServletResponse response) {
+    public Map<String,Object> doGet(HttpServletResponse response) {  //这种方式只有chrome支持 返回json类型
 
         Map<String,Object> map = new HashMap<String,Object>();
-        map.put("result","cross_result");
+        map.put("result","cross_result"+new Date().getTime());
 
         //response.setHeader("Access-Control-Allow-Origin","*"); //允许所有跨域
         response.setHeader("Access-Control-Allow-Origin","http://localhost:8590"); //允许某一域名访问
         return map;
+    }
+
+    @ResponseBody
+    @RequestMapping(value="crossWithoutHeader",method=RequestMethod.GET)
+    public String doCrossWithoutHeader(HttpServletResponse response) {  //这种方式通用 返回jsonp类型
+        String json = "{\"result\": \"crossWithoutHeader_result"+new Date().getTime()+"\"}";
+        return "data("+json + ")";
     }
 }
