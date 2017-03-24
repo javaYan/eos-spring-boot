0672455f0593bb9f30aaa66402c399d353e4b88f
javaYan <834595596@qq.com>
2017-03-24 18:57:10 +0800

    update

--- eos-java-practice/src/test/java/eos/java/practice/map/HashMapTest.java
+++ eos-java-practice/src/test/java/eos/java/practice/map/HashMapTest.java
@@ -84,7 +84,7 @@ public class HashMapTest {
 
     @Test
     public void testIndexFor() {
-        int length = 16;  //if length = 2^n , then h &(length-1) return h when h < (length-1) else return h%(length-1)
+        int length = 16;  //if length = 2^n , then h &(length-1) return h%(length-1)
         for(int h = 0; h < 130; h ++) {
             System.out.println(h & (length-1));
         }
