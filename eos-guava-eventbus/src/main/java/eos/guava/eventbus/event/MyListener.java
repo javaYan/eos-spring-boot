1fd1563498ca8ec98842db1ac741c3aec6d55053
javaYan <yanyuyu@xiaomi.com>
2017-05-09 19:30:58 +0800

    修改guava eventbus subscribe并发

--- eos-guava-eventbus/src/main/java/eos/guava/eventbus/event/MyListener.java
+++ eos-guava-eventbus/src/main/java/eos/guava/eventbus/event/MyListener.java
@@ -1,5 +1,6 @@
 package eos.guava.eventbus.event;
 
+import com.google.common.eventbus.AllowConcurrentEvents;
 import com.google.common.eventbus.DeadEvent;
 import com.google.common.eventbus.Subscribe;
 import eos.guava.eventbus.common.EventListener;
@@ -15,21 +16,25 @@ public class MyListener implements EventListener{
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    @AllowConcurrentEvents
     @Subscribe
     public void onDeleteEvent(DeleteEvent event) {
         logger.info("listener receive longEvent -" + event);
     }
 
+    @AllowConcurrentEvents
     @Subscribe
     public void onPutEvent(PutEvent event) {
         logger.info("listener receive mapEvent -" + event);
     }
 
+    @AllowConcurrentEvents
     @Subscribe
     public void onPostEvent(PostEvent event) {
         logger.info("listener receive myEvent : id-[{}], data-[{}]", event.getId(), event.getData());
     }
 
+    @AllowConcurrentEvents
     @Subscribe
     public void onMyEvent(DeadEvent event) {
         logger.info("listener receive uncaught event -" + event);
