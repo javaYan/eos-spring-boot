ecc9cb48d687e685431dd47ece8b05a38e8d7999
javaYan <yanyuyu@xiaomi.com>
2017-08-29 21:18:36 +0800

    update eos-webview-demo

--- eos-java-practice/src/main/java/eos/java/practice/ppt/divideconquer/Hanoi.java
+++ eos-java-practice/src/main/java/eos/java/practice/ppt/divideconquer/Hanoi.java
@@ -4,5 +4,10 @@ package eos.java.practice.ppt.divideconquer;
  * 汉诺塔：始终保证小圆盘上不能放大圆盘，一次只能移动一个圆盘
  */
 public class Hanoi {
-    
+    //有 A B C三个容器　　A上放入N个圆盘
+    //目标是借助B容器，将A容器上的所有圆盘移动到C容器上
+    //每次取A中最大的圆盘，移到C中
+    public void moveDisc() {
+        
+    }
 }
---
+++ eos-java-practice/src/main/java/eos/java/practice/ppt/divideconquer/Steps.java
@@ -0,0 +1,8 @@
+package eos.java.practice.ppt.divideconquer;
+
+
+public class Steps {
+    public void moveDisc() {
+        
+    }
+}
