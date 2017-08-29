c455b7c67d9da1751ceaeecb353911b4422266a9
javaYan <834595596@qq.com>
2017-08-29 23:08:22 +0800

    add LogicOperation

--- eos-java-practice/src/main/java/eos/java/practice/ppt/divideconquer/Hanoi.java
+++ eos-java-practice/src/main/java/eos/java/practice/ppt/divideconquer/Hanoi.java
@@ -4,10 +4,22 @@ package eos.java.practice.ppt.divideconquer;
  * 汉诺塔：始终保证小圆盘上不能放大圆盘，一次只能移动一个圆盘
  */
 public class Hanoi {
+    private static int count = 1 ;
     //有 A B C三个容器　　A上放入N个圆盘
     //目标是借助B容器，将A容器上的所有圆盘移动到C容器上
     //每次取A中最大的圆盘，移到C中
-    public void moveDisc() {
-        
+    public static void moveDisc(int n, char a, char b, char c) {
+        if( n == 1) {
+            System.out.println("第" + (count++) + "步 :" + "将  盘子 1 从  " + a + " 移动到  " + c);
+        } else {
+            moveDisc(n - 1, a, c, b);
+            System.out.println("第" + (count++) + "步 :" + "将  盘子 "+ n + " 从  " + a + " 移动到  " + c);
+            moveDisc(n - 1, b, a, c);
+        }
+    }
+
+
+    public static void main(String args[]) {
+        Hanoi.moveDisc(4, 'A', 'B', 'C');
     }
 }
--- eos-java-practice/src/main/java/eos/java/practice/ppt/divideconquer/Steps.java
+++ eos-java-practice/src/main/java/eos/java/practice/ppt/divideconquer/Steps.java
@@ -2,4 +2,65 @@ package eos.java.practice.ppt.divideconquer;
 
 
 public class Steps {
+
+    //直接递归
+    public int doStepsF1(int n) {
+        if( n == 1) {
+            return 1;
+        }
+        if(n == 2) {
+            return 2;
+        }
+        return doStepsF1(n-1)+doStepsF1(n-2);
+    }
+
+    private int[] temp;
+    //存储每步递归结果
+    public int doStepsF2(int n) {
+        if( n == 1) {
+            return 1;
+        }
+        if(n == 2) {
+            return 2;
+        }
+        if(temp[n] > 0) {
+            System.out.println("命中辅助存储器"+n);
+            return temp[n];
+        }
+        temp[n] = doStepsF2(n-1) + doStepsF2(n-2) ;
+        return temp[n];
+    }
+
+    //正序思考结果
+    public int doStepsF3(int n) {
+        int count = 0;
+        int before1 = 2;
+        int before2 = 1;
+        if( n == 1) {
+            return 1;
+        }
+        if(n == 2) {
+            return 2;
+        }
+        for(int i = 3; i <= n; i ++) {
+            count = before1 + before2;
+            before2 = before1;
+            before1 = count;
+        }
+        return count;
+    }
+
+
+    public static void main(String args[]) {
+        Steps steps = new Steps();
+
+        System.out.println("爬楼方法：" + steps.doStepsF1(12));
+
+
+        steps.temp = new int[12+1];
+        System.out.println("爬楼方法：" + steps.doStepsF2(12));
+
+        System.out.println("爬楼方法：" + steps.doStepsF3(12));
+    }
+
 }
